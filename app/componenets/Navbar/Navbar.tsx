0de6dcbeab6fbd9c3ed521e0b6902195f2ab267a
unknown <prantokih42@gmail.com>
2024-12-19 23:59:03 +0600

    new route created

--- app/componenets/Navbar/Navbar.tsx
+++ app/componenets/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router";
 
 const Navbar = () => {
@@ -6,7 +5,7 @@ const Navbar = () => {
     <div className="flex gap-11 justify-center " >
       <Link to="/"><button className="btn bg-red-500">Home</button></Link>
       <Link to="/redux"><button className="btn bg-blue-500">Redux</button></Link>
-      <button className="btn bg-green-500">demo</button>
+      <Link to="/demo"><button className="btn bg-green-500">demo</button></Link>
     </div>
   );
 };
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  // Define the index route
   index("routes/home.tsx"),
-
-  // Define a route for Redux
   route("redux", "routes/redux.tsx"),
+  route("demo", "routes/demo.tsx"),
 ] satisfies RouteConfig;
