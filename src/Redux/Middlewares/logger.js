4a2aa37000f9383e0b10b37e99839f269ff57b89
unknown <prantokih42@gmail.com>
2025-01-19 22:56:05 +0600

    redux practice done

--- src/Redux/Middlewares/logger.js
+++ src/Redux/Middlewares/logger.js
@@ -1,4 +1,9 @@
 const logger  = (store) => (next) => (action) => {
    const currentState = store.getState();
+   console.log("current State =>",currentState );
+   console.log("action dispatched=>",action);
+   
+   next(action);
+   console.log("updated state =>",store.getState());
 }
 export default logger
