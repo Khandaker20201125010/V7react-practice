5c276804948593386e9c2a9295250eb031b2afab
unknown <prantokih42@gmail.com>
2025-01-20 23:30:59 +0600

    logger added

--- src/Redux/Middlewares/logger.js
+++ src/Redux/Middlewares/logger.js
@@ -2,7 +2,7 @@ const logger  = (store) => (next) => (action) => {
    const currentState = store.getState();
    console.log("current State =>",currentState );
    console.log("action dispatched=>",action);
-   
+   //logeer
    next(action);
    console.log("updated state =>",store.getState());
 }
